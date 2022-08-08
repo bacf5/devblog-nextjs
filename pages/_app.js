60ad9b687f3d15c33117db25526bb9cc8a837b02
bacf5 <brunoagustincarusofassa@gmail.com>
2022-08-07 22:17:27 -0300

    persistent footer with links

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css';
 import Header from '../src/components/Header';
 import Link from 'next/link';
+import Footer from '../src/components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </div>
   );
 }
